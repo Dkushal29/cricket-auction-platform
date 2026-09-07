ffe07f34aa0318ef043b6dc42657b4cf3a478bd8
D Kushal <dkushal29082005@gmail.com>
2026-09-07 15:22:37 +0530

    Configure webpack path alias for Render

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
+import path from "path";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config) => {
+    config.resolve.alias["@"] = path.resolve(process.cwd());
+    return config;
+  },
   async headers() {
     return [
       {
